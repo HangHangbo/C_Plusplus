f9a60198e821cef398031ec62d3d025f6d44898f
HangHangbo <1679759074@qq.com>
2019-03-22 21:35:34 +0800

    niuke

--- niuke/niuke/test.cpp
+++ niuke/niuke/test.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-using namespace std;
-#include<string>
+//#include<iostream>
+//using namespace std;
+//#include<string>
 
 
 
@@ -117,67 +117,209 @@ int main()
 //你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
 
 
-//class Solution {
-//public:
-//	string addStrings(string num1, string num2) {
-//		
-//
-//	}
-//};
-
-//翻转字符串
-
-string reverseString(string s)
-{
-	size_t start = 0;
-	size_t end = s.size() - 1;
-	while (start < end){
-		swap(s[start], s[end]);
-		start++;
-		end--;
-	}
-	return s;
-}
-
-
-//字符串中的一个唯一的字符.1
-
 class Solution {
 public:
-	int firstUniqChar(string s) {
-		int count[26] = { 0 };
-		int size = s.size();
-		for (auto& e : s){
-			count[e-'a']++;
+	string addStrings(string num1, string num2) {
+		int len1 = num1.length()-1;
+		int len2 = num2.length()-1;
+		string num3;  
+		int carray = 0;
+		int sum = 0;
+		while (len1 >= 0 || len2 >= 0) {
+			int a = num1[len1] - '0';
+			int b = num2[len2] - '0';
+			 sum = 0;
+				sum+= carray;
+			if (len1 >= 0){
+				sum += a;
+			}
+			if (len1 >= 0){
+				sum += b;
+			}
+			if (sum > 10){
+				carray = 1;
+				sum -= 10;
+			}
+			num3 += sum + '0';
+
+
+			//int sum = a + b + carray;
+			//carray = sum >= 10 ? 1 : 0;
+			//sum = sum - 10;   //sum的范围为0~18 %运算比较耗时 使用 -更方便
+			//char c = '0' + sum; //将当前为转为字符型
+			//num3 += c;         //逆行存储
+			////int sum;
+			////num3.insert(0, 1, sum + '0');  //头插
+			len1--;
+			len2--;
 		}
-		for (int i = 0; i < s.size(); i++){
-			if (count[s[i]-'a'] == 1)
-				return i;
+	/*	if (len1 == 0) {
+			for (int i = len2; i > 0; i--) {
+				int b = num2[i - 1] - '0';
+				int sum = b + carray;
+				carray = sum >= 10 ? 1 : 0;
+				sum = sum % 10;
+				char c = '0' + sum;
+				num3 += c;
+			}
+			if (carray == 1) num3 += '1';
 		}
-		return -1;
+		else if (len2 == 0) {
+			for (int i = len1; i > 0; i--) {
+				int b = num1[i - 1] - '0';
+				int sum = b + carray;
+				carray = sum >= 10 ? 1 : 0;
+				sum = sum % 10;
+				char c = '0' + sum;
+				num3 += c;
+			}
+			if (carray == 1) num3 += '1';
+		}*/
+		//反转
+		int len3 = num3.length();
+		for (int i = 0; i <= (len3 - 1) / 2; i++) {
+			char c = num3[i];
+			num3[i] = num3[len3 - 1 - i];
+			num3[len3 - 1 - i] = c;
+		}
+		return num3;
 	}
 };
+
+////翻转字符串
+//
+//string reverseString(string s)
+//{
+//	size_t start = 0;
+//	size_t end = s.size() - 1;
+//	while (start < end){
+//		swap(s[start], s[end]);
+//		start++;
+//		end--;
+//	}
+//	return s;
+//}
+
+
+////字符串中的一个唯一的字符.1
+//
+//class Solution {
+//public:
+//	int firstUniqChar(string s) {
+//		int count[26] = { 0 };
+//		int size = s.size();
+//		for (auto& e : s){
+//			count[e-'a']++;
+//		}
+//		for (int i = 0; i < s.size(); i++){
+//			if (count[s[i]-'a'] == 1)
+//				return i;
+//		}
+//		return -1;
+//	}
+//};
 
 
 //字符串中的一个唯一的字符.2
 
+#include<iostream>
+using namespace std;
+#include<string>
+//int main()
+//{	string s;
+//	while (getline(cin,s))
+//	{
+//		int hashtable[256] = { 0 };
+//		bool flag = false;
+//		for (auto& e : s)
+//		{
+//			hashtable[e]++;
+//		}
+//
+//		for (int i = 0; i < s.size(); i++)
+//		{
+//			if (hashtable[s[i]] == 1){
+//				cout << s[i] << endl;
+//				flag = true;
+//				break;
+//			}
+//		}
+//		if (flag == false)
+//			cout << "-1" << endl;
+//	}
+//	return 0;
+//}
 
 
 
+////输出最后一个单词的长度
+//
+//int main()
+//{
+//	string s;
+//	while (cin >> s){
+//		size_t pos = s.rfind(' ');
+//		if (pos == string::npos){
+//			cout << s.size() << endl;
+//		}
+//		else{
+//			cout << s.size() - pos - 1 << endl;
+//		}
+//		}
+//	return 0;
+//}
 
-//输出最后一个单词的长度
+////给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+////说明：本题中，我们将空字符串定义为有效的回文串。
+//class Solution {
+//public:
+//	bool Isnumberorletter(char x){
+//		
+//		return (x > '0' && x<'9') || (x>'a' && x<'z') || (x>'A'&&x < 'Z');
+//			;
+//	}
+//	bool isPalindrome(string s) {
+//		if (s.empty()){
+//			return true;
+//		}
+//		int begin = 0;
+//		int end = s.size() - 1;
+//		//将小写转为大写
+//		/*for (auto &e: s){
+//			if (e>= 'a')
+//			e-=32;
+//		}*/
+//		while (begin < end){
+//			while (begin != end){                 //从前向后找第一个字符
+//				if (Isnumberorletter(s[begin])){
+//					break;
+//					++begin;
+//				}
+//				while (end != begin){             //从后向前找第一个字符
+//					if (Isnumberorletter(s[end]))
+//						break;
+//					--end;
+//				}
+//				if (begin < end)
+//				{
+//					// 检测begin和end位置字符是否相等 或者是否不区分大小的相等
+//					//大小写字母码值相差32   先加上32 后再减去 ‘a’ 数字就会变为负数  %32 后大小写相同
+//				
+//				if((s[begin]+32-'a')%32!=(s[end]+32-'a')%32)
+//					
+//
+//			//小写换打写后直接比较
+//			/*	if (s[begin] != s[end])*/
+//
+//
+//					return false;
+//					++begin;
+//					--end;
+//				}
+//
+//				return true;
+//			}
+//		}
+//	}
+//};
 
-int main()
-{
-	string s;
-	while (cin >> s){
-		size_t pos = s.rfind(' ');
-		if (pos == string::npos){
-			cout << s.size() << endl;
-		}
-		else{
-			cout << s.size() - pos - 1 << endl;
-		}
-		}
-	return 0;
-}
