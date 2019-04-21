65a556bb528b87d10d7e7898004e36c7fce966b3
HangHangbo <1679759074@qq.com>
2019-04-21 23:14:03 +0800

    带环链表判断

--- 回文链表/回文链表/test.cpp
+++ 回文链表/回文链表/test.cpp
@@ -9,36 +9,56 @@ using namespace std;
      ListNode(int x) : val(x), next(NULL) {}
  };
 
-class Solution {
-public:
-	bool isPalindrome(ListNode* head) {
-		if (head == NULL || head->next == NULL)
-		{
-			return true;
-		}
-		ListNode *cur = head;
-		ListNode *ret = head;
-		while (ret)
-		{
-			cur = cur->next;
-			ret = ret->next->next;
-		}
-		stack<ListNode*> s;
-		while (cur)
-		{
-			ListNode *tmp = cur;
-			cur = cur->next;
-			s.push(tmp);
-		}
-		ListNode *point = head;
-		while (s.top())
-		{
-			if (point != s.top())
-			{
-				return false;
-			}
-			s.pop();
-		}
-		return true;
-	}
-};
+ class Solution {
+ public:
+	 bool isPalindrome(ListNode* head) {
+		 if (head == nullptr || head->next == nullptr) return true;
+		 ListNode *fast = head;
+		 ListNode *slow = head;
+		 stack<int> s;
+		 s.push(head->val);
+		 while (fast->next&&fast->next->next)
+		 {
+			 fast = fast->next->next;
+			 slow = slow->next;
+			 s.push(slow->val);
+		 }
+		 if (!fast->next) s.pop();
+		 while (slow->next) {
+			 slow = slow->next;
+			 int tmp = s.top(); s.pop();
+			 if (tmp != slow->val) return false;
+		 }
+		 return true;
+	/*	 if (!head || !head->next) return true;
+		 ListNode *slow = head, *fast = head;
+		 stack<int> s;
+		 s.push(head->val);
+		 while (fast->next && fast->next->next) {
+			 slow = slow->next;
+			 fast = fast->next->next;
+			 s.push(slow->val);
+		 }
+		 if (!fast->next) s.pop();
+		 while (slow->next) {
+			 slow = slow->next;
+			 int tmp = s.top(); s.pop();
+			 if (tmp != slow->val) return false;
+		 }
+		 return true;*/
+	 }
+ };
+
+ int main()
+ {
+	 Solution s;
+	 ListNode *s1 = new ListNode(1);
+	 ListNode *s2 = new ListNode(0);
+	 ListNode *s3 = new ListNode(0);
+	 s1->next = s2;
+	 s2->next = s3;
+	 s3->next = nullptr;
+	 cout << s.isPalindrome(s1) << endl;
+	 system("pause");
+	 return 0;
+ }
